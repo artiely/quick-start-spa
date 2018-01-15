c375bb7c53d9373ff67b94a6e28f254082f0a784
artiely <1119696785@qq.com>
2018-01-15 16:39:11 +0800

    自动按本机ip启动

--- config/index.js
+++ config/index.js
@@ -3,6 +3,9 @@
 // see http://vuejs-templates.github.io/webpack for documentation.
 
 const path = require('path')
+var address = require('address')
+var _localhost = address.ip()
+console.log('本机ip', _localhost)
 
 module.exports = {
   dev: {
@@ -13,7 +16,7 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
+    host: (_localhost || 'localhost'), // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,

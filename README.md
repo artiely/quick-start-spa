cde4904bd40f8dfadba46f9eaca86d15e01d328e
artiely <1119696785@qq.com>
2018-01-15 18:17:00 +0800

    初始vuex+router

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 1.加入启动自动以本机ip启动，方便移动端调试   
 2.对axios进行简单封装,可以拦截,取消请求,请求统一管理   
-3.自动安装常用依赖less,sass,stylus,axios...     
+3.自动安装常用依赖axios,js-cookie,less,sass,stylus,...     
 4.打包后查看资源大小仪表图by `webpack-bundle-analyzer`    
 ```
 npm run dist
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,48 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+const HelloWorld = () =>
+  import('@/components/HelloWorld')
 
 Vue.use(Router)
-
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+Router.prototype.back = () => {
+  this.isBack = true
+  window.history.go(-1)
+}
+const router = new Router({
+  scrollBehavior (to, from, savedPosition) {
+    return {
+      x: 0,
+      y: 0
     }
-  ]
+  },
+  routes: [{
+    path: '/',
+    name: 'HelloWorld',
+    component: HelloWorld
+  }]
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth) {
+    // 登录验证
+    /* let token = null
+    if (token) {
+      next({
+        query: {
+          redirect: to.fullPath
+        }
+      })
+    } else {
+      next({
+        path: '/login',
+        query: {
+          redirect: to.fullPath
+        }
+      })
+    } */
+  } else {
+    next()
+  }
+})
+
+export default router

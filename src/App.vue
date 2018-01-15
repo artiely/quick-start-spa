fdacc722fc40b843729cd9c12f93599647a08437
artiely <1119696785@qq.com>
2018-01-15 18:29:39 +0800

    添加默认路由动画

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,69 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <transition :name="transitionName">
+    <keep-alive>
+      <router-view/>
+    </keep-alive>
+    </transition>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      transitionName: 'slide-left'
+    }
+  },
+    watch: {
+    '$route': {
+      handler(val, oval) {
+        let isBack = this.$router.isBack
+        if (isBack) {
+          this.transitionName = 'leave'
+          if (val.path === '/login') {
+            this.$router.push('/index')
+          }
+        } else {
+          this.transitionName = 'enter'
+        }
+        this.$router.isBack = false
+      }
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="less">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.slide-left-enter {
+  opacity: 0;
+  -webkit-transform: translate(100%, 0);
+  transform: translate(100%, 0);
+}
+
+.slide-right-leave-active {
+  opacity: 0;
+  -webkit-transform: translate(100%, 0);
+  transform: translate(100%, 0);
+}
+
+.slide-right-enter {
+  opacity: 0;
+  -webkit-transform: translate(0, 0);
+  transform: translate(0 0);
+}
+.slide-left-leave-active {
+  opacity: 0;
+  -webkit-transform: translate(0, 0);
+  transform: translate(0 0);
 }
 </style>
